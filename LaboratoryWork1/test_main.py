2cd3ef1cb9ecc94c20005188534d30a87287fb20
Иван Виноградов <149311203+IvanStriker@users.noreply.github.com>
2025-09-13 18:44:32 +0300

    Update test_main.py
    More test added to test_main.py

--- LaboratoryWork1/test_main.py
+++ LaboratoryWork1/test_main.py
@@ -1,10 +1,48 @@
 import unittest
-from main import searchingTwo
+from searcher import *
 
 
 class TestMath(unittest.TestCase):
-    def test_add_positive(self):
-        self.assertEqual(searchingTwo([1, 2, 3, 4, 5], 3), [0, 1])
+    def test1STD(self):
+        self.assertEqual(Searcher.searchTwoWithSum([1, 2, 2, 1, 5], 3), [0, 1])
+
+    def test2WrongInputValue(self):
+        self.assertEqual(Searcher.searchTwoWithSum([], 1, True), None)
+
+    def test3WrongInputValue(self):
+        with self.assertRaises(SearcherInputValueError):
+            Searcher.searchTwoWithSum([], 5)
+
+    def test4WrongInputValue(self):
+        with self.assertRaises(SearcherInputValueError):
+            Searcher.searchTwoWithSum([], 5)
+
+    def test5WrongInputValue(self):
+        with self.assertRaises(SearcherInputValueError):
+            Searcher.searchTwoWithSum([1], 5)
+
+    def test6WrongInputType(self):
+        self.assertEqual(Searcher.searchTwoWithSum((1, 2, 3), 3, True), None)
+
+    def test7WrongInputType(self):
+        with self.assertRaises(SearcherInputTypeError):
+            Searcher.searchTwoWithSum((1, 2, 3), 3)
+
+    def test8WrongInputType(self):
+        with self.assertRaises(SearcherInputTypeError):
+            Searcher.searchTwoWithSum([1, 2, "4"], 3)
+
+    def test9WrongInputType(self):
+        with self.assertRaises(SearcherInputTypeError):
+            Searcher.searchTwoWithSum([1, 2.6, 3], 4)
+
+    def test10WrongInputType(self):
+        with self.assertRaises(SearcherInputTypeError):
+            Searcher.searchTwoWithSum([1, 2, 3], "3")
+
+    def test11STD(self):
+        self.assertEqual(Searcher.searchTwoWithSum([1, 2, 2, 1, 5, 2], 4), [1, 2])
+
 
 
 if "__name__" == "__main__":
