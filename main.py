b3cc8695f1ae015ac73f8bf8f3f158d92b62d4c3
Иван Виноградов <149311203+IvanStriker@users.noreply.github.com>
2025-09-13 16:30:44 +0300

    First commit. Function searchingTwo added

---
+++ main.py
@@ -0,0 +1,15 @@
+def searchingTwo(nums: list, target: int):
+    fstAcc = {}
+    for i in range(len(nums)):
+        if nums[i] in fstAcc:
+            fstAcc[nums[i]] += [i]
+        else:
+            fstAcc[nums[i]] = [i]
+    for i in range(len(nums)):
+        if (target - nums[i]) in fstAcc:
+            indexes = fstAcc[target - nums[i]]
+            if indexes[0] != i:
+                return [i, indexes[0]]
+            elif len(indexes) > 1:
+                return [i, indexes[1]]
+    return None
